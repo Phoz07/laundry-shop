81a7f055481811fa270e79d55bc8b1f874efeec8
Phoz07 <maverxk07@gmail.com>
2026-02-21 13:06:49 +0700

    optimize

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,19 +16,19 @@
         name="keywords"
         content="ซักรีดเสื้อผ้า, บริการซักรีดเสื้อผ้า, ซักรีดเสื้อผ้า, ร้านซักรีด"
     />
-    <meta name="author" content="Your Name" />
+    <meta name="author" content="Smith Laundry" />
 
     <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website" />
+    <meta property="og:type" content="website" /> 
     <meta property="og:title" content="Smith Laundry - บริการซักรีดเสื้อผ้า" />
     <meta
         property="og:description"
         content="Smith Laundry ร้านซักรีดเสื้อผ้าที่ให้บริการด้วยความใส่ใจ ผ้าสะอาด หอม นุ่ม พร้อมส่งมอบความสดใสให้กับทุกชิ้นผ้าของคุณ"
     />
-    <!-- <meta property="og:image" content="/og-image.jpg" /> -->
+    <meta property="og:image" content="/images/result1.png" />
 
     <!-- Canonical URL -->
-    <link rel="canonical" href="https://yourdomain.com" />
+    <link rel="canonical" href="https://laundry-shop.pages.dev/" />
 </svelte:head>
 
 {@render children()}
